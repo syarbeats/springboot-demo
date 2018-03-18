51409a95f28243ebb24f22139d2b1c23e1c05a5c
syarbeats <srf.hidayat@gmail.com>
2018-03-18 17:57:28 +0700

    Add loggging in to PersonController

--- src/main/java/com/cdc/mitrais/springbootdemoone/SpringbootDemoOneApplication.java
+++ src/main/java/com/cdc/mitrais/springbootdemoone/SpringbootDemoOneApplication.java
@@ -1,5 +1,7 @@
 package com.cdc.mitrais.springbootdemoone;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -8,9 +10,13 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class SpringbootDemoOneApplication extends SpringBootServletInitializer{
+	
+	
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SpringbootDemoOneApplication.class, args);
+		
 	}
 	
 	@Override
--- src/main/java/com/cdc/mitrais/springbootdemoone/controller/PersonController.java
+++ src/main/java/com/cdc/mitrais/springbootdemoone/controller/PersonController.java
@@ -2,13 +2,14 @@ package com.cdc.mitrais.springbootdemoone.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 
 import com.cdc.mitrais.springbootdemoone.model.Person;
 import com.cdc.mitrais.springbootdemoone.service.IPersonService;
@@ -20,12 +21,17 @@ public class PersonController {
 	@Autowired
 	IPersonService personService;
 	
+	private static final Logger logger = LoggerFactory.getLogger(PersonController.class);
+	
 	@GetMapping("persons")
 	public ResponseEntity<List<Person>> getAllPersons() {
 		List<Person> list = personService.getAllData();
+		logger.debug("PERSON-Controller-url(/user/persons): GetAllData()");
+		logger.info("PERSON-Controller-url(/user/persons): GetAllData()");
 		
 		for(Person person : list) {
-			System.out.println("Person Data:"+ person.getFirstName());
+			logger.debug("Person Data:"+ person.getFirstName());
+			logger.info("Person Data:"+ person.getFirstName());
 		}
 		
 		return new ResponseEntity<List<Person>>(list, HttpStatus.OK);
